a30d06753b3affae29e7c2ecce8ed1e02934e3cf
Wladimir Bandeira <tecnico100@gmail.com>
2023-02-06 14:49:42 -0300

    criando estrutura para simular para saber se está conectado na internet ou não

--- lib/app/presentation/modules/splash/views/splash_view.dart
+++ lib/app/presentation/modules/splash/views/splash_view.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:stream/app/data/repositories_implementation/connectivity_repository_impl.dart';
+import 'package:stream/app/domain/repositories/connectivity_repository.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    ConnectivityRepository connectivityRepository =
+        ConnectivityRepositoryImpl();
+    final hasInternet = await connectivityRepository.hasInternet;
+    if (hasInternet) {
+      print("O valor da variável é $hasInternet");
+    } else {}
+  }
 
   @override
   Widget build(BuildContext context) {
