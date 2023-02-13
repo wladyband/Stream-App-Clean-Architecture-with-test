7ad0f65593c0530051e7f764f25be7b5e47c6d75
Wladimir Bandeira <tecnico100@gmail.com>
2023-02-13 15:07:58 -0300

    evolução da tela de acesso, porém ela está com bug

--- lib/app/data/repositories_implementation/authentication_repository_impl.dart
+++ lib/app/data/repositories_implementation/authentication_repository_impl.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:stream/app/domain/either.dart';
+import 'package:stream/app/domain/models/enums.dart';
 import 'package:stream/app/domain/models/user.dart';
 import 'package:stream/app/domain/repositories/authentication_repository.dart';
 
@@ -18,5 +20,23 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
   Future<bool> get isSignedIn async {
     final sessionId = await _secureStorage.read(key: _key);
     return sessionId != null;
+  }
+
+  @override
+  Future<Either<SignFailure, User>> signIn(
+    String username,
+    String password,
+  ) async {
+    //TODO;
+    await Future.delayed(const Duration(seconds: 6));
+    if (username != 'test') {
+      return Either.left(SignFailure.notFound);
+    }
+    if (password != '123456') {
+      return Either.left(SignFailure.unauthorized);
+    }
+    return Either.right(
+      User(),
+    );
   }
 }
--- lib/app/domain/repositories/authentication_repository.dart
+++ lib/app/domain/repositories/authentication_repository.dart
@@ -1,6 +1,12 @@
+import 'package:stream/app/domain/either.dart';
+import 'package:stream/app/domain/models/enums.dart';
 import 'package:stream/app/domain/models/user.dart';
 
 abstract class AuthenticationRepository {
   Future<bool> get isSignedIn;
   Future<User?> getUserData();
+  Future<Either<SignFailure, User>> signIn(
+    String username,
+    String password,
+  );
 }
